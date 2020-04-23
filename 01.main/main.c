cb608b577d7d024c144e223c3814bc6858208917
Dasha Shcheglova <shcheglovadasha02@gmail.com>
2020-04-23 15:03:58 +0300

    calc.vector

--- 01.main/main.c
+++ 01.main/main.c
@@ -2,89 +2,136 @@
  ============================================================================
  Name        : calculator.c
  Author      : Dasha Shcheglova
- Version     :
+ Version     : 2.0
  Copyright   :
- Description : calculator
+ Description : calculator.vector
  ============================================================================
  */
 #include <stdio.h>
 #include <stdlib.h>
 int main(void)  //Calculator function for selecting an operation
 {
-	int a;
-	int b;
-	char c;
-	float result;
-	int i;
+	int *vector_a;
+	int *vector_b;
+	char c=0;
+	float *vector_result;
+	int i=0;
+	int size=0;
 
 	do
 	{
+		puts("Enter the size of vectors: ");
+		scanf("%i", &size);
        printf("Select an operation ");
-       printf("Operation (+,-,/,*,!,^,z): ");   //prints operations list on the screen, z - exit
+       printf("Operation (+,-,/,*,z): ");   //prints operations list on the screen, z - exit
 	   scanf(" %c", &c);  //entering operations from the keyboard
 	switch (c)
 	{
 	case '+':                /* finds a+b */
+		vector_a = calloc(size, sizeof(int));
 		printf("First number: ");
-		scanf("%i", &a);
-		printf("Second number: ");
-		scanf("%i", &b);
-		result = a + b;
-		printf("Answer: %i + %i = %f \n", a, b, result);
-		break;
-	case '-':                /* finds a-b */
-		printf("First number: ");
-		scanf("%i", &a);
-		printf("Second number: ");
-		scanf("%i", &b);
-		result = a - b;
-		printf("Answer: %i - %i = %f \n", a, b, result);
-		break;
-    case '/':               /* finds a/b */
-		printf("First number: ");
-		scanf("%i", &a);
-		printf("Second number: ");
-		scanf("%i", &b);
-		result = a / b;
-		printf("Answer: %i / %i = %f \n", a, b, result);
-		break;
-	case '*':               /* finds a*b */
-		printf("First number: ");
-		scanf("%i", &a);
-		printf("Second number: ");
-		scanf("%i", &b);
-		printf("Answer: %i * %i = %f \n", a, b, result);
-		break;
-	case '!':                 /* factorial */
-		printf("First number: ");
-	    scanf("%i", &a);
-		if (a == 0)
-		{
-			result = 1;
+		for (i = 0; i < size; i++) {
+			printf("vector_a[%i]=", i);
+			scanf("%i", &vector_a[i]);
 		}
-		else
-		{
-		result = 1;
-		for (i = 1; i <= a; i++)
-		   {
-            result = result *i;
-		   }
+		vector_b = calloc(size, sizeof(int));
+		printf("Second number: ");
+		for (i = 0; i < size; i++) {
+			printf("vector_b[%i]=", i);
+			scanf("%i", &vector_b[i]);
 		}
-		printf("Answer: %i! = %f \n", a, result);
-		break;
-	case '^':            /* exponentiation */
-	    printf("First number: ");
-	    scanf("%i", &a);
-	    printf("Second number: ");
-	    scanf("%i", &b);
-	    result = 1;
 
-	    for (i= 0; i < b; i++)
-	    {
-	    	result = result * a;
-	    }
-	    printf("Answer: %i ^ %i = %f \n", a, b, result);
+		vector_result = calloc(size, sizeof(float));
+		for (i = 0; i < size; i++) {
+			vector_result[i] = vector_a[i] + vector_b[i];
+		}
+		printf("Answer: ");
+		for (i = 0; i < size; i++) {
+			printf("vector_result[%i] = %f \n", i, vector_result[i]);
+		}
 		break;
+		free(vector_a);
+		free(vector_b);
+		free(vector_result);
+
+	case '-':                /* finds a-b */
+		vector_a = calloc(size, sizeof(int));
+				printf("First number: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_a[%i]=", i);
+					scanf("%i", &vector_a[i]);
+				}
+				vector_b = calloc(size, sizeof(int));
+				printf("Second number: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_b[%i]=", i);
+					scanf("%i", &vector_b[i]);
+				}
+
+				vector_result = calloc(size, sizeof(float));
+				for (i = 0; i < size; i++) {
+					vector_result[i] = vector_a[i] - vector_b[i];
+				}
+				printf("Answer: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_result[%i] = %f \n", i, vector_result[i]);
+				}
+				break;
+				free(vector_a);
+				free(vector_b);
+				free(vector_result);
+    case '/':               /* finds a/b */
+    	vector_a = calloc(size, sizeof(int));
+    			printf("First number: ");
+    			for (i = 0; i < size; i++) {
+    				printf("vector_a[%i]=", i);
+    				scanf("%i", &vector_a[i]);
+    			}
+    			vector_b = calloc(size, sizeof(int));
+    			printf("Second number: ");
+    			for (i = 0; i < size; i++) {
+    				printf("vector_b[%i]=", i);
+    				scanf("%i", &vector_b[i]);
+    			}
+
+    			vector_result = calloc(size, sizeof(float));
+    			for (i = 0; i < size; i++) {
+    				vector_result[i] = vector_a[i] / vector_b[i];
+    			}
+    			printf("Answer: ");
+    			for (i = 0; i < size; i++) {
+    				printf("vector_result[%i] = %f \n", i, vector_result[i]);
+    			}
+    			break;
+    			free(vector_a);
+    			free(vector_b);
+    			free(vector_result);
+	case '*':               /* finds a*b */
+		vector_a = calloc(size, sizeof(int));
+				printf("First number: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_a[%i]=", i);
+					scanf("%i", &vector_a[i]);
+				}
+				vector_b = calloc(size, sizeof(int));
+				printf("Second number: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_b[%i]=", i);
+					scanf("%i", &vector_b[i]);
+				}
+
+				vector_result = calloc(size, sizeof(float));
+				for (i = 0; i < size; i++) {
+					vector_result[i] = vector_a[i] * vector_b[i];
+				}
+				printf("Answer: ");
+				for (i = 0; i < size; i++) {
+					printf("vector_result[%i] = %f \n", i, vector_result[i]);
+				}
+				break;
+				free(vector_a);
+				free(vector_b);
+				free(vector_result);
 	case 'z':
 	        printf("Program exit");
 	        break;
